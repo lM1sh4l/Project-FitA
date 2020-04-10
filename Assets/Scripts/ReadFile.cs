1b59a213d6b5b7722cbf3ba4c66671667dda3741
DewittSmith <dewittsmith1337@gmail.com>
2020-04-10 23:30:04 +0700

    Create ReadFile.cs

---
+++ Assets/Scripts/ReadFile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+/*public enum Language
+{
+    russian,
+    english
+};*/
+
+public class ReadFile : MonoBehaviour
+{
+    public string path;
+    //public Language language;
+    [TextArea(1, 10)]
+    public List<string> quotes;
+
+    void Awake()
+    {
+        if (File.Exists(path))
+        {
+            string[] lines = File.ReadAllLines(path);
+            foreach(var v in lines)
+            {
+                if (v != "") quotes.Add(v);
+            }
+        }
+    }
+}
