d8d149c6a1dd7ba5855c791a90ee9bb62b78c7bc
DewittSmith <dewittsmith1337@gmail.com>
2020-04-11 15:09:30 +0700

    readfile

--- Assets/Scripts/ReadFile.cs
+++ Assets/Scripts/ReadFile.cs
@@ -1,29 +1,42 @@
 ﻿using UnityEngine;
-using System.IO;
 using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+using System;
 
-/*public enum Language
+public enum Language
 {
     russian,
     english
-};*/
+};
 
 public class ReadFile : MonoBehaviour
 {
     public string path;
-    //public Language language;
+    public Language language;
     [TextArea(1, 10)]
-    public List<string> quotes;
+    public string[] quotes;
 
-    void Awake()
+    void OnValidate()
     {
-        if (File.Exists(path))
+        TextAsset text = Resources.Load(path) as TextAsset;
+        XDocument document = XDocument.Parse(text.text);
+        List<XElement> elements = document.Root.Elements("object").ToList();
+        XElement element = elements.Find(i => i.Attribute("name").Value == Enum.GetName(typeof(Language), language));
+        quotes = element.Value.Split('\n');
+        for (int i = 0; i < quotes.Length; ++i)
         {
-            string[] lines = File.ReadAllLines(path);
-            foreach(var v in lines)
+            int remove = 0;
+            for (int j = 0; j < quotes[i].Length; ++j)
             {
-                if (v != "") quotes.Add(v);
+                if (char.IsWhiteSpace(quotes[i][j]))
+                {
+                    ++remove;
+                }
+                else break;
             }
+            quotes[i] = quotes[i].Remove(0, remove);
         }
+        quotes = quotes.Where(i => i != "").ToArray();
     }
 }
